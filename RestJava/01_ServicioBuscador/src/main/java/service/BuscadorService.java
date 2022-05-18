c06ed3ab93c69ee92f61afe85b90a23423b13d57
AndresCuichan <andrescuichanflores@gmail.com>
2022-05-18 10:31:10 +0200

    hasta el ejercicio 1

---
+++ RestJava/01_ServicioBuscador/src/main/java/service/BuscadorService.java
@@ -0,0 +1,15 @@
+package service;
+
+import java.util.List;
+
+import model.Pagina;
+
+public interface BuscadorService {
+	List<Pagina> buscar(String tematica);
+	void alta(Pagina pagina);
+	List<Pagina> paginas();
+	
+	void eliminar(String tematica);
+	Pagina actualizar(Pagina pagina);
+	void actualizarTematica(String direccion, String tematica);
+}
